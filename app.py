f1540baddac02e26e69505c38f1d48c458637ea5
Zfeng0207 <160209304+Zfeng0207@users.noreply.github.com>
2024-08-12 23:09:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ from pandasai import SmartDataframe
 from langchain_community.llms import Ollama
 from langchain_groq.chat_models import ChatGroq
 from openai import AsyncOpenAI
+from pandasai.llm import OpenAI
 
 # groq key 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
 # Set the environment variable
 
 # Initialize the model with the API key
-api_key ='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'
-llm = ChatGroq(model_name='llama3-70b-8192', api_key=api_key)
-
+llm = OpenAI(api_token="OPENAIKEY")
 @cl.on_chat_start
 def start_chat():
     # set initial message history
@@ -26,10 +25,8 @@ async def main(message: cl.Message):
     # Retrieve message history
     message_history = cl.user_session.get("message_history")
     message_history.append({"role": "user", "content": message.content})
-
-    data = pd.read_csv('raw_timesheet.csv')
+    data = 'ms_cleaned.csv'
     df = SmartDataframe(data,config = {'llm':llm})
-
     question = message.content
     response = df.chat(question)
     msg = cl.Message(content=response)
